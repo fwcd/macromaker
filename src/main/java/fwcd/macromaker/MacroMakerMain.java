be255e703a49832d93583b9a9b0ec20150a6b4a5
fwcd <fwcdmail@gmail.com>
2019-02-13 21:33:59 +0100

    Bridged JUL logging to SLF4J

--- src/main/java/fwcd/macromaker/MacroMakerMain.java
+++ src/main/java/fwcd/macromaker/MacroMakerMain.java
@@ -1,11 +1,24 @@
 package fwcd.macromaker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
 import fwcd.fructose.swing.PanelFrame;
+import fwcd.fructose.time.Stopwatch;
 import fwcd.macromaker.ui.MacroMakerMenuBar;
 import fwcd.macromaker.ui.MacroMakerViewController;
 
 public class MacroMakerMain {
+	private static final Logger LOG = LoggerFactory.getLogger(MacroMakerMain.class);
+	
 	public static void main(String[] args) {
+		SLF4JBridgeHandler.removeHandlersForRootLogger();
+		SLF4JBridgeHandler.install();
+		
+		Stopwatch watch = new Stopwatch();
+		watch.start();
+		
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MacroMaker");
 		
@@ -14,5 +27,7 @@ public class MacroMakerMain {
 		frame.setJMenuBar(new MacroMakerMenuBar(vc).getComponent());
 		frame.revalidate();
 		frame.setAlwaysOnTop(true);
+		
+		LOG.info("Launched application in {} ms", watch.stop());
 	}
 }
